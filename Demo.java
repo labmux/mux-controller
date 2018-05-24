7313b1aa5b6136456e499698d829784a6689ecd0
Eliran Assaraf <eliran.a.bg@gmail.com>
2018-05-24 17:19:45 -0400

    update 3.2
    got rid of menu class

--- Demo.java
+++ Demo.java
@@ -5,7 +5,39 @@ import java.util.Scanner;
 public class Demo {
 
 	public static void main(String[] args) {
-		Menu menu = new Menu();
+		int subwalls;
+		int height, width;
+		Scanner kb = new Scanner(System.in);
+		int input = 1;
+		
+		do {
+		System.out.println("How many subwalls?");
+		subwalls = kb.nextInt();
+		
+		System.out.println("Grid height");
+		height = kb.nextInt();
+		System.out.println("Grid width");
+		width = kb.nextInt();
+		
+		int[][] grid = new int[height][width];
+		
+		switch (subwalls) 
+		{
+			case 1: 
+				MidArray mid = new MidArray(grid);
+				
+				System.out.println(mid);
+				break;
+			case 2:
+					ArrayShape shape = new ArrayShape(grid);
+					System.out.println(shape);
+					shape.test();
+					break;
+		}
+		System.out.println("\nWould you like to try another grid? "
+				+ "(1 = yes/0 = no)");
+		input = kb.nextInt();
+		} while(input == 1);
 
 	}
 }
